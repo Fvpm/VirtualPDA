d0e0a29ced17d3c10b0918ee127a51a0d30052d0
DarthRen27 <91697042+DarthRen27@users.noreply.github.com>
2021-10-06 14:27:25 -0500

    Methods, inputs, and name

---
+++ Classes.py
@@ -0,0 +1,103 @@
+"""
+Currently coded for Python only, not coded to work with database
+
+"""
+
+class DataObjects(object):
+    def __init__(self, ident):
+        self.id = ident
+        self.update = False
+        self.mark = False
+        
+class Note(DataObjects):
+    
+    def __init__(self, own, visib, made, mod, memo, eday, impor, name, col, repeat, tag):
+        self.owner = own
+        self.vis = [visib]
+        self.dmade = made
+        self.lmod = mod
+        self.text = memo
+        self.edate = eday
+        self.imp = impor
+        self.title = name
+        self.color = col
+        self.rep = repeat
+        self.tags = [tag]
+        
+    def edit(self, entertext):
+        self.text += entertext
+        
+    def delete(self):
+        """delete the note"""
+        
+    def share(self, shareuser):
+        """share note with other users"""
+        self.vis.append(shareuser)
+        
+class User(DataObjects):
+    def __init__(self, intuse, intpass):
+        self.username = intuse
+        self.password = intpass
+        self.groups = []
+        self.notes = []
+        
+    def changePass(self, newpassword):
+        self.password = newpassword
+        
+    def checkPass(self, attempt):
+        if attempt == self.password:
+            return True
+        else:
+            return False
+        
+    def createNote(self, date, entry):
+        newnote=Note(self, self.id, date, date, entry)
+        self.notes.append(newnote)
+        
+    def changeUser(self, newusername):
+        self.username = newusername
+        
+    def joinGroup(self, group):
+        """Join a group"""
+        self.groups.append(group.id)
+        
+    def createGroup(self, groupname, desc):
+        """Create a new group"""
+        newgroup = Group(groupname, desc, self.id)
+        self.group.append(newgroup)
+        
+    def leaveGroup(self, groupid):
+        self.group.remove(groupid)
+        
+    def delete(self):
+        """delete the user"""
+    
+class Group(DataObjects):
+    def __init__(self, groupname, desc, own):
+        self.name = groupname
+        self.description = desc
+        self.owner = own
+        self.isPrivate = True
+        self.members = [own]
+        
+    def addUser(self, newuser):
+        self.members.append(newuser)
+        
+    def editDesc(self, newdesc):
+        self.description = newdesc
+        
+    def remUser(self, memind):
+        self.members.pop(memind)
+        
+    def editName(self, newname):
+        self.name = newname
+        
+    def togglePrivacy(self):
+        """Change the privacy setting of the group"""
+        if (self.isPrivate == True):
+            self.isPrivate == False
+        else:
+            self.isPrivate == True
+        
+    def delete(self):
+        """Delete the group"""
