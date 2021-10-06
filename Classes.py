4a7f25753af430f436d36d21177c12793849783e
DarthRen27 <91697042+DarthRen27@users.noreply.github.com>
2021-10-06 16:22:28 -0500

    Update Classes.py
    Added tests to the bottom, and for some reason the compiler was saying the subclasses didn't have an id attribute, so temporarily removed them

--- Classes.py
+++ Classes.py
@@ -60,15 +60,15 @@ class User(DataObjects):
         
     def joinGroup(self, group):
         """Join a group"""
-        self.groups.append(group.id)
+        self.groups.append(group)
         
     def createGroup(self, groupname, desc):
         """Create a new group"""
         newgroup = Group(groupname, desc, self)
-        self.group.append(newgroup)
+        self.groups.append(newgroup)
         
     def leaveGroup(self, groupid):
-        self.group.remove(groupid)
+        self.groups.remove(groupid)
         
     def delete(self):
         """delete the user"""
@@ -102,3 +102,44 @@ class Group(DataObjects):
         
     def delete(self):
         """Delete the group"""
+        
+def main():
+    user1 = User("Jason", "x1pho3nc0rp")
+    user2 = User("Kylie", "t3n3m4n")
+    note1 = Note(user1, user1, "0", "0", "", "0", 3, "Alexander Fails", "Red", False, "Junk")
+    group1 = Group("Alexander Fan Club", "For people who love Alexander the Great", user1)
+    
+    print(user1.checkPass("x1pho3nc0rp"))
+    user1.changePass("sh4d0wKn1gh7")
+    user1.changeUser("Alex")
+    print(user1.password)
+    print(user1.username)
+    user1.joinGroup(group1)
+    print(user1.groups)
+    user1.leaveGroup(group1)
+    print(user1.groups)
+    #print(user1.notes)
+    #user1.createNote("0", "Behold the Empire")
+    #print(user1.notes)
+    user1.createGroup("Titanic", "For those who love Titanic")
+    user1.joinGroup(group1)
+    print(user1.groups)
+    group1.addUser(user2)
+    print(group1.members)
+    group1.remUser(user1)
+    print(group1.members)
+    group1.editName("Rolling")
+    group1.editDesc("For those who want to roll dice")
+    print(group1.description)
+    print(group1.name)
+    print(group1.isPrivate)
+    group1.togglePrivacy()
+    print(group1.isPrivate)
+    print(note1.vis)
+    note1.share(user2)
+    print(note1.vis)
+    note1.edit("The cake is a lie")
+    print(note1.text)
+    
+
+main()
