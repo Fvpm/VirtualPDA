45e6ae54ca99197ec05a3511266863e484e589af
DarthRen27 <91697042+DarthRen27@users.noreply.github.com>
2021-10-13 17:42:52 -0500

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -4,6 +4,7 @@ Currently coded for Python only, not coded to work with database
 """
 
 import mysql.connector
+import datetime
 
 class DataObjects(object):
     def __init__(self, ident):
@@ -28,7 +29,28 @@ class Note(DataObjects):
         
     def edit(self, entertext):
         """Currently only allows adding to text, will eventually allow for full editing of text. expected input is string"""
-        self.text += entertext
+        update_notedata = ("UPDATE notes"
+                        "SET notedata = %s"
+                        "WHERE note_id = %s")
+        update_notedate = ("UPDATE notes"
+                        "SET lastmod = %s"
+                        "WHERE note_id = %s")
+        cursor.execute(update_notedata, entertext, self.id)
+        cursor.execute(update_notedate, datetime.now().date(), self.id)
+        
+    def addTag(self, newtag, tagtext):
+        """Adds a new tag to the list of tags for the note. Expected inputs are int and string"""
+        self.tags.append([newtag, tagtext])
+        add_tag = ("INSERT INTO tags"
+                        "(tag_id, tag_text, note_id)"
+                        "VALUES (%(tag_id)s, %(tag_text)s, %(note_id)s)")
+        cursor.execute(add_tag, newtag, tagtext, self.id)
+        
+    def removeTag(self, oldtag):
+        """Removes a tag from the list of tags. Expected inputs are list"""
+        self.tags.remove(oldtag)
+        remove_tag = ("DELETE FROM tags WHERE tag_id = %s AND note_id = %s")
+        cursor.execute(remove_tag, oldtag[0], self.id)
         
     def delete(self):
         """delete the note"""
@@ -44,6 +66,10 @@ class Note(DataObjects):
     def share(self, shareuser):
         """share note with other users. expected input is int"""
         self.vis.append(shareuser)
+        add_usercon = ("INSERT INTO usercon"
+                        "(user_id, note_id)"
+                        "VALUES (%(user_id)s, %(note_id)s)")
+        cursor.execute(add_usercon, shareuser, self.id)
         
 class User(DataObjects):
     def __init__(self, intuse, intpass):
@@ -57,7 +83,7 @@ class User(DataObjects):
         self.password = newpassword
         modify_pass = ("UPDATE users"
                        "SET password = %s"
-                       "WHERE userId = %s")
+                       "WHERE user_id = %s")
         cursor.execute(modify_pass, newpassword, self.id)
         
     def checkPass(self, attempt):
@@ -71,6 +97,14 @@ class User(DataObjects):
         """create a new note. expected inputs are strings"""
         newnote=Note(self.id, self, date, date, entry, "", 0, "", "", False, "")
         self.notes.append(newnote)
+        add_note = ("INSERT INTO notes"
+                        "(note_id, user_id, date_made, lastmod, notedata, date, import, title, color, repeat)"
+                        "VALUES (%(note_id)s, %(user_id)s, %(date_made)s, %(lastmod)s, %(notedata)s, %(date)s, %(import)s, %(title)s, %(color)s, %(repeat)s)")
+        add_usercon = ("INSERT INTO usercon"
+                        "(user_id, note_id)"
+                        "VALUES (%(user_id)s, %(note_id)s)")
+        cursor.execute(add_note, noteid, self.id, date, date, entry, "", 5, "", "", False)
+        cursor.execute(add_usercon, self.id, noteid)
         
     def changeUser(self, newusername):
         """allows user to change their username. must be logged in before they can change username. expected input is string"""
@@ -83,15 +117,33 @@ class User(DataObjects):
     def joinGroup(self, group):
         """Join a group. expected input is int"""
         self.groups.append(group)
+        add_groupmem = ("INSERT INTO groupmem"
+                        "(group_id, user_id)"
+                        "VALUES (%(group_id)s, %(user_id)s)")
+        cursor.execute(add_groupmem, group, self.id)
         
-    def createGroup(self, groupname, desc):
+    def createGroup(self, groupname, desc, groupid, noteid):
         """Create a new group. expected inputs are strings"""
         newgroup = Group(groupname, desc, self)
         self.groups.append(newgroup)
+        new_group = ("INSERT INTO groups"
+                     "(group_id, name, description, user_id, privacy)"
+                     "VALUES (%(group_id)s, %(name)s, %(description)s, %(user_id)s, %(privacy)s)")
+        add_groupmem = ("INSERT INTO groupmem"
+                        "(group_id, user_id)"
+                        "VALUES (%(group_id)s, %(user_id)s)")
+        add_groupcon = ("INSERT INTO groupcon"
+                        "(group_id, note_id)"
+                        "VALUES (%(group_id)s, %(note_id)s)")
+        cursor.execute(new_group, groupid, groupname, desc, self.id, True)
+        cursor.execute(add_groupmem, groupid, self.id)
+        cursor.execute(add_groupcon, groupid, noteid)
         
     def leaveGroup(self, groupid):
         """removes user from a group voluntarily. expected input is int"""
         self.groups.remove(groupid)
+        remove_self=("DELETE FROM groupmem WHERE user_id = %s")
+        cursor.execute(remove_self, self.id)
         
     def delete(self):
         """delete the user"""
@@ -117,18 +169,32 @@ class Group(DataObjects):
     def addUser(self, newuser):
         """add a new user to the group. expected input is int"""
         self.members.append(newuser)
+        add_groupmem = ("INSERT INTO groupmem"
+                        "(group_id, user_id)"
+                        "VALUES (%(group_id)s, %(user_id)s)")
+        cursor.execute(add_groupmem, self.id, newuser)
         
     def editDesc(self, newdesc):
         """edit the description of the group. expected input is string"""
         self.description = newdesc
+        modify_desc = ("UPDATE groups"
+                       "SET description = %s"
+                       "WHERE group_id = %s")
+        cursor.execute(modify_desc, newdesc, self.id)
         
     def remUser(self, memind):
         """remove a user from the group. expected input is int"""
         self.members.remove(memind)
+        remove_user=("DELETE FROM groupmem WHERE user_id = %s")
+        cursor.execute(remove_user, memind)
         
     def editName(self, newname):
         """edit the name of the group. expected input is string"""
         self.name = newname
+        modify_name = ("UPDATE groups"
+                       "SET name = %s"
+                       "WHERE group_id = %s")
+        cursor.execute(modify_name, newname, self.id)
         
     def togglePrivacy(self):
         """Change the privacy setting of the group"""
@@ -136,6 +202,10 @@ class Group(DataObjects):
             self.isPrivate == False
         else:
             self.isPrivate == True
+        modify_privacy = ("UPDATE groups"
+                       "SET privacy = %s"
+                       "WHERE group_id = %s")
+        cursor.execute(modify_privacy, self.isPrivate, self.id)
         
     def delete(self):
         """Delete the group"""
